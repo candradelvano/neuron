ecf58659a64c305e3fd765f737294e6973b756a4
unknown <candra@neuronworks.co.id>
2022-06-04 11:20:41 +0700

    overloading

--- 03/herodocnewdoc.php
+++ 03/herodocnewdoc.php
@@ -4,11 +4,19 @@ $pesanKonfirmasi = "Selamat, Candra Delvano\n
 Anda Berhak Mendapatkan Hadiah Mobile ABCD";
 echo $pesanKonfirmasi;
 
+//herodoc
 $name = "Candra Delvano";
 $pesan = <<< TEXT
 Selamat, $name
 Anda Berhak Mendapatkan Hadiah Mobile ABCD
 Silahkan Hubungi Admin
 TEXT;
+echo $pesan;
 
+// newdoc
+$pesan = <<<'TEXT'
+Selamat, $name
+Anda Berhak Mendapatkan Hadiah Mobile ABCD
+Silahkan Hubungi Admin
+TEXT;
 echo $pesan;
