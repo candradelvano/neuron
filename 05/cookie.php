8804b26c18b09a2ee7b114eaa9aee9cc3036904f
unknown <candra@neuronworks.co.id>
2022-06-17 17:36:48 +0700

    fungsi upload file

--- 05/cookie.php
+++ 05/cookie.php
@@ -1,5 +1,6 @@
 <?php
     setcookie("last_login",date("Y-m-d H:i:s"),time()-3600);
-    $cookie_name = "user";
-    $cookie_value = "Alex Porter";
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
+    echo $_COOKIE['last_login'];
+    setcookie("last_login",'',time()+3600,'/');
+    echo "<br>";
+    echo $_COOKIE['last_login'];
