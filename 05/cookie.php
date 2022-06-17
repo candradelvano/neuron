a27d4aa7ae2f54bd9ce8d01b3f6db4c57216fe2d
unknown <candra@neuronworks.co.id>
2022-06-17 17:50:57 +0700

    fungsi upload file

--- 05/cookie.php
+++ 05/cookie.php
@@ -1,6 +1,14 @@
 <?php
-    setcookie("last_login",date("Y-m-d H:i:s"),time()-3600);
+    /* setcookie("last_login",date("Y-m-d H:i:s"),time()-3600);
     echo $_COOKIE['last_login'];
     setcookie("last_login",'',time()+3600,'/');
     echo "<br>";
-    echo $_COOKIE['last_login'];
+    echo $_COOKIE['last_login']; */
+?>
+
+<?php
+    $dsn = 'mysql:host=localhost;dbname=test';
+    $pdo = new PDO($dsn, 'root', '');
+    $stm = $pdo->prepare("INSERT INTO test (name, created_at) VALUES (?,?)");
+    $stm->execute(['candra delvano',date("Y-m-d H:i:s")]);
+?>
